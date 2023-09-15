b20135bd05751edfd8c21656b6dd4058f7c8437b
Johnny Filipe <John.Filipe@lshtm.ac.uk>
2023-09-15 21:29:31 +0100

    lm fit; new death date var; plot codes simplified

--- analysis/040_plot_hospitalisations.R
+++ analysis/040_plot_hospitalisations.R
@@ -13,35 +13,12 @@ fs::dir_create(output_dir_plot)
 
 shielding_cohort <- arrow::read_parquet(file = here::here("output/data_edited.gz.parquet"),
                                         compression = "gzip", compression_level = 5)
-shielding_cohort_s <- shielding_cohort
-shielding_cohort_a <- shielding_cohort
-shielding_cohort_r <- shielding_cohort
-
-shielding_cohort_s %>% group_by(shielding) %>% #by shielding
-  summarise(hosps = sum(all_covid_hosp, na.rm = T)) %>% print()
-
-shielding_cohort_a %>% group_by(age_cat) %>% #by age_cat
-  summarise(hosps = sum(all_covid_hosp, na.rm = T)) %>% print()
-
-shielding_cohort_r %>% group_by(practice_nuts) %>% #by practice_nuts
-  summarise(hosps = sum(all_covid_hosp, na.rm = T)) %>% print()
-
 
 # Add week number and year for first hospitalisation
 shielding_hosp <- shielding_cohort %>% 
-  dplyr::select(patient_id,
+  dplyr::select(patient_id,		
                 dplyr::starts_with("pt_"),
-                dplyr::contains("hosp_admitted")) %>% 
-  pivot_longer(cols = dplyr::contains("hosp_admitted"), 
-               names_pattern = "covid_hosp_admitted_(.)",
-               names_to = "covid_admission",
-               values_to = "admission_date") %>% 
-  drop_na()
-  
-shielding_hosp_s <- shielding_cohort_s %>% 
-  dplyr::select(patient_id,
-                dplyr::starts_with("pt_"),
-                shielding,
+                shielding, age_cat, practice_nuts,					
                 dplyr::contains("hosp_admitted")) %>% 
   pivot_longer(cols = dplyr::contains("hosp_admitted"), 
                names_pattern = "covid_hosp_admitted_(.)",
@@ -49,145 +26,96 @@ shielding_hosp_s <- shielding_cohort_s %>%
                values_to = "admission_date") %>% 
   drop_na()
 
-shielding_hosp_a <- shielding_cohort_a %>% 
-  dplyr::select(patient_id,
-                dplyr::starts_with("pt_"),
-                age_cat,
-                dplyr::contains("hosp_admitted")) %>% 
-  pivot_longer(cols = dplyr::contains("hosp_admitted"), 
-               names_pattern = "covid_hosp_admitted_(.)",
-               names_to = "covid_admission",
-               values_to = "admission_date") %>% 
-  drop_na()
-  
-  shielding_hosp_r <- shielding_cohort_r %>% 
-  dplyr::select(patient_id,
-                dplyr::starts_with("pt_"),
-                practice_nuts,
-                dplyr::contains("hosp_admitted")) %>% 
-  pivot_longer(cols = dplyr::contains("hosp_admitted"), 
-               names_pattern = "covid_hosp_admitted_(.)",
-               names_to = "covid_admission",
-               values_to = "admission_date") %>% 
-  drop_na()
-
-# create numeric week and year variables to group on
-shielding_hosp <- shielding_hosp %>%
-  mutate(hosp_week = lubridate::week(admission_date),
-         hosp_year = lubridate::year(admission_date))
-		 
-shielding_hosp_s <- shielding_hosp_s %>%
-  mutate(hosp_week = lubridate::week(admission_date),
-         hosp_year = lubridate::year(admission_date))
-
-shielding_hosp_a <- shielding_hosp_a %>%
-  mutate(hosp_week = lubridate::week(admission_date),
-         hosp_year = lubridate::year(admission_date))
-
-shielding_hosp_r <- shielding_hosp_r %>%
-  mutate(hosp_week = lubridate::week(admission_date),
-         hosp_year = lubridate::year(admission_date))
-
-#WEEKLY
-shielding_hosp_summ <- shielding_hosp %>% 
-  group_by(hosp_year, hosp_week) %>%
-  summarise(weekly_admissions = n()) %>% 
-  ungroup()
-  
-  shielding_hosp_summ_s <- shielding_hosp_s %>% 
-  group_by(hosp_year, hosp_week, shielding) %>%
-  summarise(weekly_admissions = n()) %>% 
-  ungroup()
-
-shielding_hosp_summ_a <- shielding_hosp_a %>% 
-  group_by(hosp_year, hosp_week, age_cat) %>%
-  summarise(weekly_admissions = n()) %>% 
-  ungroup()
-
-shielding_hosp_summ_r <- shielding_hosp_r %>% 
-  group_by(hosp_year, hosp_week, practice_nuts) %>%
-  summarise(weekly_admissions = n()) %>% 
-  ungroup()
-
+# create week and year variables
 mindate <- min(shielding_hosp$admission_date, na.rm = TRUE)
 print(mindate)
-shielding_hosp_summ <- shielding_hosp_summ %>% 
-  mutate(plot_date = mindate + weeks(hosp_week - week(mindate)) + years(hosp_year - year(mindate))) %>% 
-  mutate(total_admissions = cumsum(weekly_admissions)) #CUMULATIVE
+  
+shielding_hosp <- shielding_hosp %>%
+  mutate(hosp_week = lubridate::week(admission_date),
+         hosp_year = lubridate::year(admission_date),
+         plot_date = mindate + weeks(hosp_week - week(mindate)) + years(hosp_year - year(mindate)))
 
-mindate <- min(shielding_hosp_s$admission_date, na.rm = TRUE)
-shielding_hosp_summ_s <- shielding_hosp_summ_s %>% 
-  mutate(plot_date = mindate + weeks(hosp_week - week(mindate)) + years(hosp_year - year(mindate))) %>% 
-  mutate(total_admissions = cumsum(weekly_admissions)) #CUMULATIVE
+#Week density and cumulative
+By_week <- function(data, vars){
+  data %>% 
+     group_by(across({{vars}}))      %>%
+     mutate(weekly_admissions = n()) %>%
+     ungroup(hosp_year, hosp_week)   %>%
+     arrange(plot_date)              %>%
+     mutate(total_admissions = cumsum(weekly_admissions)) %>% #CUMULATIVE                                  
+     ungroup()
+}
 
-shielding_hosp_summ_a <- shielding_hosp_summ_a %>% 
-  mutate(plot_date = mindate + weeks(hosp_week - week(mindate)) + years(hosp_year - year(mindate))) %>% 
-  mutate(total_admissions = cumsum(weekly_admissions)) #CUMULATIVE
+#overall
+shielding_hosp_summ   <- shielding_hosp %>% By_week(c(hosp_year, hosp_week))
+# by shielding
+shielding_hosp_summ_s <- shielding_hosp %>% By_week(c(hosp_year, hosp_week, shielding))
+# by age
+shielding_hosp_summ_a <- shielding_hosp %>% By_week(c(hosp_year, hosp_week, age_cat))                         
+# by region
+shielding_hosp_summ_r <- shielding_hosp %>% By_week(c(hosp_year, hosp_week, practice_nuts))
 
-shielding_hosp_summ_r <- shielding_hosp_summ_r %>% 
-  mutate(plot_date = mindate + weeks(hosp_week - week(mindate)) + years(hosp_year - year(mindate))) %>% 
-  mutate(total_admissions = cumsum(weekly_admissions)) #CUMULATIVE
 
 
-##WEEKLY
+#Figures - overlapping groups
+fig0 <- function(data, x , y, col, xname='Date', yname='Weekly admissions') {
+  ggplot(data, aes(x = {{x}}, y = {{y}}, col = {{col}})) +
+    geom_line() + 
+    geom_point(size = 1.2, pch = 1) +
+    labs(x = xname, y = yname) +
+    ylim(c(0, NA)) +
+    theme_bw()
+}
+#Figures - panels for groups
+fig <- function(data, x , y, col, facets, xname='Date', yname='Weekly admissions') {
+  ggplot(data, aes(x = {{x}}, y = {{y}}, col = {{col}})) +
+    geom_line() + 
+    geom_point(size = 1.2, pch = 1) +
+    facet_wrap(facets, ncol = 1, scales = 'free_y') +
+    labs(x = xname, y = yname) +
+	ylim(c(0, NA)) +
+    theme_bw()
+}
+
+##PLOTS WEEKLY
 pdf(here::here("output/figures/covid_hosp_over_time2.pdf"), width = 8, height = 6)
 ##All
-ggplot(shielding_hosp_summ, aes(x = plot_date, y = weekly_admissions)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
-#by shielding
-ggplot(shielding_hosp_summ_s, aes(x = plot_date, y = weekly_admissions, col = shielding)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~shielding, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
+fig(shielding_hosp_summ,   plot_date, weekly_admissions,                      facets = 'NULL',          xname = 'Date',  yname = 'Weekly admissions')
+
+#by shielding - overlapping
+fig0(shielding_hosp_summ_s, plot_date, weekly_admissions, col = shielding,     xname = 'Date',  yname = 'Weekly admissions')
 #by age_cat
-ggplot(shielding_hosp_summ_a, aes(x = plot_date, y = weekly_admissions, col = age_cat)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~age_cat, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
-#by practice_nuts
-ggplot(shielding_hosp_summ_r, aes(x = plot_date, y = weekly_admissions, col = practice_nuts)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~practice_nuts, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
+fig0(shielding_hosp_summ_a, plot_date, weekly_admissions, col = age_cat,       xname = 'Date',  yname = 'Weekly admissions')
+#by region
+fig0(shielding_hosp_summ_r, plot_date, weekly_admissions, col = practice_nuts, xname = 'Date',  yname = 'Weekly admissions')
+
+#by shielding - panels
+fig(shielding_hosp_summ_s, plot_date, weekly_admissions, col = shielding,     facets = "shielding",     xname = 'Date',  yname = 'Weekly admissions')
+#by age_cat
+fig(shielding_hosp_summ_a, plot_date, weekly_admissions, col = age_cat,       facets = "age_cat",       xname = 'Date',  yname = 'Weekly admissions')
+#by region
+fig(shielding_hosp_summ_r, plot_date, weekly_admissions, col = practice_nuts, facets = "practice_nuts", xname = 'Date',  yname = 'Weekly admissions')
+
 dev.off()
 
 
-#CUMULATIVE
+##PLOTS CUMULATIVE
 pdf(here::here("output/figures/covid_hosp_over_time_cumsum2.pdf"), width = 8, height = 6)
 ##All
-ggplot(shielding_hosp_summ, aes(x = plot_date, y = total_admissions)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
-#by shielding
-ggplot(shielding_hosp_summ_s, aes(x = plot_date, y = total_admissions, col = shielding)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~shielding, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw() 
+fig(shielding_hosp_summ,   plot_date, total_admissions,                      facets = 'NULL',          xname = 'Date',  yname = 'Total admissions')
+
+#by shielding - overlapping
+fig0(shielding_hosp_summ_s, plot_date, total_admissions, col = shielding,     xname = 'Date',  yname = 'Total admissions')
 #by age_cat
-ggplot(shielding_hosp_summ_a, aes(x = plot_date, y = total_admissions, col = age_cat)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~age_cat, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
-#by practice_nuts
-ggplot(shielding_hosp_summ_r, aes(x = plot_date, y = total_admissions, col = practice_nuts)) +
-  geom_line() + 
-  geom_point(size = 1.2, pch = 1) +
-  facet_wrap(~practice_nuts, ncol = 1, scales = "free_y") +
-  labs(x = "Date", y = "Weekly admissions") + 
-  theme_bw()
+fig0(shielding_hosp_summ_a, plot_date, total_admissions, col = age_cat,       xname = 'Date',  yname = 'Total admissions')
+#by region
+fig0(shielding_hosp_summ_r, plot_date, total_admissions, col = practice_nuts, xname = 'Date',  yname = 'Total admissions')
+
+#by shielding - panels
+fig(shielding_hosp_summ_s, plot_date, total_admissions, col = shielding,     facets = "shielding",     xname = 'Date',  yname = 'Total admissions')
+#by age_cat
+fig(shielding_hosp_summ_a, plot_date, total_admissions, col = age_cat,       facets = "age_cat",       xname = 'Date',  yname = 'Total admissions')
+#by region
+fig(shielding_hosp_summ_r, plot_date, total_admissions, col = practice_nuts, facets = "practice_nuts", xname = 'Date',  yname = 'Total admissions')
+
 dev.off()
