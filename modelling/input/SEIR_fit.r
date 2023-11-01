9c7d33b18a8ccf179e0d9f87463ba70071593465
Johnny Filipe <John.Filipe@lshtm.ac.uk>
2023-11-01 20:42:50 +0000

    SEIUHRD_data and _fit revision

--- modelling/input/SEIR_fit.r
+++ modelling/input/SEIR_fit.r
@@ -1,38 +1,61 @@
 
-#Range of dates (weeks) for fitting
+#Time range (week-dates) and subsets of datasets for model fitting
 if (pset$iplatform==0) {
   iweeksmodel = seq_along(datM$Weeks)
   iweeksdata  = iweeksmodel
   #Data within the time range
-  zd = datM$Dataz[iweeksdata] 
+  zd = datM$Dataz[iweeksdata]     #weekly incidence in model
   if (pset$imodel==2) {
-  wd = datM$Dataw[iweeksdata] }
+  wd = datM$Dataw[iweeksdata] 
+  print(paste0("#H data pts fitted: ", length(iweeksmodel)))
+  print(paste0("#D data pts fitted: ", length(iweeksmodel))) 
+  } else { 
+  print(paste0("#data pts fitted: ", length(iweeksmodel))) }
 
-} else { #weekly incidence in model
+} else {
   if (pset$iplatform==1) Week2ofStudy = Week2OfModel #Dummy data: use max of contact matrix range
   if (pset$iplatform==2) Week2ofStudy = "2020-12-01" #Real data: use start of vacc & alpha
-  Week1OfFit = max(c(Week1OfData, Week1OfModel))
-  Week2OfFit = min(c(Week2OfData, Week2OfModel, Week2ofStudy))
-  #Dates within the time rangeH
-  iweeksdata = which(Week1OfFit <= datD$DatesH & datD$DatesH <= Week2OfFit)
-  Dates = datD$DatesH[iweeksdata]
-      #iweeksdataH = which(Week1OfFit <= datD$DatesH & datD$DatesH <= Week2OfFit)
-      #DatesH = datD$DatesH[iweeksdataH]
-      #iweeksdataD = which(Week1OfFit <= datD$DatesD & datD$DatesD <= Week2OfFit)
-      #DatesD = datD$DatesD[iweeksdataD]
-  #convert data-weeks to model-weeks (indices 1:52) counted from start week of model
-  iweeksmodel = ceiling(as.numeric(difftime(Dates, Week1OfModel, units = "weeks")))
+  if (pset$imodel==1) {
+    Week1OfFit  = max(c(unique(datD$Week1OfData),  Week1OfModel)) #Week#Ofmodel from SEIR_contacts
+    Week2OfFit  = min(c(unique(datD$Week2OfData),  Week2OfModel, Week2ofStudy)) } else {
+    Week1OfFitH = max(c(unique(datDH$Week1OfData), Week1OfModel)) #Week#Ofmodel from SEIR_contcats
+    Week2OfFitH = min(c(unique(datDH$Week2OfData), Week2OfModel, Week2ofStudy))
+    Week1OfFitD = max(c(unique(datDD$Week1OfData), Week1OfModel))
+    Week2OfFitD = min(c(unique(datDD$Week2OfData), Week2OfModel, Week2ofStudy)) }
+  #Dates within the time range
+  if (pset$imodel==1) {
+    iweeksdata  = which(Week1OfFit  <= datDH$Dates & datDH$Dates <= Week2OfFitH)
+    Dates = datDH$Dates[iweeksdataH] } else {
+    iweeksdataH = which(Week1OfFitH <= datDH$Dates & datDH$Dates <= Week2OfFitH)
+    iweeksdataD = which(Week1OfFitD <= datDD$Dates & datDD$Dates <= Week2OfFitD)
+    DatesH = datDH$Dates[iweeksdataH]
+    DatesD = datDD$Dates[iweeksdataD] }
   #Data within the time range
-  zd = datD$Dataz[iweeksdata]           #weekly incidence of hospital admissions 
-  if (pset$imodel==2) {
-  wd = datD$Dataw[iweeksdata] }         #weekly incidence of deaths
-}
+  if(pset$imodel==1) {
+    zd = datD$Dataz [iweeksdata]  } else {
+    zd = datDH$Dataz[iweeksdataH]           #weekly incidence of hospital admissions 
+    wd = datDD$Dataw[iweeksdataD] }         #weekly incidence of deaths  
+
+  #convert Dates-weeks to model-weeks (indices 1:52) counted from start week of model
+  #=> used in likelihood
+  if(pset$imodel==1) {
+    iweeksmodel  = ceiling(as.numeric(difftime(Dates,  Week1OfModel, units = "weeks")))
+    print(paste0("#data pts fitted: ", length(iweeksmodel)))
+  } else if (pset$imodel==2) {
+    iweeksmodelH = ceiling(as.numeric(difftime(DatesH, Week1OfModel, units = "weeks")))
+    iweeksmodelD = ceiling(as.numeric(difftime(DatesD, Week1OfModel, units = "weeks")))
+    print(paste0("#H data pts fitted: ", length(iweeksmodelH)))
+    print(paste0("#D data pts fitted: ", length(iweeksmodelD)))     }
+
+  } #iplatform #Time range 
+
 
 ######## 1 BayesianTools #####################
 ### reproducibility of MCMC sample
 set.seed(7777)
 ### number of estimated parameters
-if(pset$iplatform==0) {thetaTrue = c(pars$rEI, pars$rIR, pars$R0, pars$pE0, pars$pdm, dev0);
+if(pset$iplatform==0) {
+   thetaTrue = c(pars$rEI, pars$rIR, pars$R0, pars$pE0, pars$pdm, dev0);
    npar = length(thetaTrue)}
 if (pset$iplatform>0) {
    npar = 6
@@ -44,9 +67,10 @@ if (pset$imodel==1) {model <- SEIR}
 if (pset$imodel==2) {model <- SEIUHRD; sd2osd1 = mean(wd)/mean(zd)}
 
 ### Likelihood in R and model in Rcpp
-sdUpper = 1 #p #binomial likelihood and data
-sdUpper = 0.4*mean(zd) #sd #normal likleidood, any data
+sdUpper = 1            #p #binomial likelihood, and data
+sdUpper = 0.4*mean(zd) #sd  #normal likelihood, any data
 
+#Model 1
 LogLikelihood1 <- function(theta){
   #Proposed
   pars$rEI = theta[1] 
@@ -63,16 +87,19 @@ LogLikelihood1 <- function(theta){
   #Model in Rcpp with proposed parameters
   m <- model(pars)
   #likelihood of (weekly) data
-  if (pset$iplatform==0) {Mean=m$Iw} else {Mean=m$Iw[iweeksmodel]}; if (Mean[1]==0) Mean[1]=1; #avoid NAs
-  mu = Mean #pars$pdm*Mean
+  if (pset$iplatform==0) {
+    Mean=m$Iw              } else {
+    Mean=m$Iw[iweeksmodel] }; 
+  if (Mean[1]==0) Mean[1]=1; #avoid NAs
+  mu = Mean  #pars$pdm*Mean
   #Negative binomial likelihood - product over weeks
     #return( sum(dnbinom(x = zd, size = 1/p, mu = mu, log=T))) #expect k=1/p=200=> p=0.005
   #Normal likelihood - product over weeks
     return(sum(dnorm(zd, mean = mu, sd = sd, log = T)))
 }
 
+#Model 2
 source(file = paste0(input_dir,"/SEIUHRD_BETA.r")) #Used within Likelihood2
-
 LogLikelihood2 <- function(theta){
   #Proposed
   pars$rEI = theta[1] 
@@ -89,23 +116,27 @@ LogLikelihood2 <- function(theta){
   #Model in Rcpp with proposed parameters
   m <- model(pars)
   #likelihood of (weekly) data
-  if (pset$iplatform==0) {MeanH=m$Hw; MeanD=m$Dw;} else {MeanH=m$Hw[iweeksmodel]; MeanD=m$Dw[iweeksmodel]}; 
-  MeanH[1]=max(MeanH[1],1); #avoid NAs
-  MeanD[1]=max(MeanD[1],1); #avoid NAs
-  muH = MeanH #pars$pdm*MeanH #
-  muD = pars$pdm*MeanD#MeanD #
+  if (pset$iplatform==0) {
+    MeanH = m$Hw; 
+    MeanD = m$Dw;               } else {
+    MeanH = m$Hw[iweeksmodelH]; 
+    MeanD = m$Dw[iweeksmodelD]  }; 
+  MeanH[1]= max(MeanH[1],1); #avoid NAs
+  MeanD[1]= max(MeanD[1],1); #avoid NAs
+  muH = MeanH                #pars$pdm*MeanH #
+  muD = pars$pdm*MeanD       #MeanD #
   sdD = sd2osd1*sdH
   #Negative binomial likelihood - product over weeks
     #return( sum(dnbinom(x = zd, size = 1/p, mu = mu, log=T))) #expect k=1/p=200=> p=0.005
   #Normal likelihood - product over weeks
-    return(sum(dnorm(zd, mean = muH, sd = sdH, log = T))+sum(dnorm(wd, mean = muD, sd = sdD, log = T)))
+    return(sum(dnorm(zd, mean = muH, sd = sdH, log = T))+  sum(dnorm(wd, mean = muD, sd = sdD, log = T)))
 }
 
 ## Likelihood definition, parameter ranges
-niter    = 200000 #120000 #90000 #60000 #150000 #30000 #50000 #40000
-if (pset$imodel==1) {LogLikelihood = LogLikelihood1; Lower=c(1,1,1,1,1,1)*0.0001; Upper = c(1,1,30,1,2,sdUpper)
-             } else {LogLikelihood = LogLikelihood2; Lower=c(1,1,1,1,1,1)*0.0001; Upper = c(1,1,30,1,1,sdUpper)}
-        #lower = c(1,1,1,1,1,1)*0.001, upper =c(1,1,30,1,1,sdUpper)) #rEI, rIR, R0, pE0, pdm, sd or p
+niter = 200000 #30000 #120000 #90000 #60000 #150000 #30000 #50000 #40000
+if (pset$imodel==1) {
+  LogLikelihood = LogLikelihood1; Lower=c(1,1,1,1,1)*0.0001;   Upper = c(1,1,30,1,sdUpper)   } else {
+  LogLikelihood = LogLikelihood2; Lower=c(1,1,1,1,1,1)*0.0001; Upper = c(1,1,30,1,2,sdUpper)} #rEI, rIR, R0, pE0, pdm, sd or p
 setup    = createBayesianSetup(likelihood=LogLikelihood, lower = Lower, upper = Upper) #parallel = T,
 settings = list (iterations = niter, burnin = round(niter*npar/15), message=F)
 ## Bayesian sample
@@ -127,17 +158,6 @@ parsE$pE0 <- MAPE$parametersMAP[4] #thetaTrue[4]
 parsE$pdm <- MAPE$parametersMAP[5] #thetaTrue[5]
 mE        <- model(parsE)
 
-###simulation
-#### True parameters - to be estimated
-#pbin = 1/200 #0.5            #for neg binomial data (size=k=200 Davies 2020 Nat Med) or binomial noise
-#sd   = round(0.05*mean(zm))  #for normal data
-#dev0= sd #pbin #sd #noise parameter
-###now
-#### True parameters - to be estimated
-#pbin = 1/pars$k #1/200 #0.5  #for neg binomial data (size=k=200 Davies 2020 Nat Med) or binomial noise
-#sd   = round(0.05*mean(zm))  #for normal data
-#dev0 = sd #pbin #sd #noise parameter
-
 #binomial fit to binomial data or normal fit to normal data
 if(pset$iplatform>0) {
    pbin = 1/pars$k;             #for neg binomial data 
@@ -145,31 +165,51 @@ if(pset$iplatform>0) {
 #Expected parameter estimates
 thetaTrue = c(pars$rEI, pars$rIR, pars$R0, pars$pE0, pars$pdm, dev0); #
 
-##sd of normal likelihood for binomial data or normal data
+##sd of normal likelihood for normal data
 #dev = round(0.05*mean(zd))
-##sd of NB data
-if (pset$imodel==1) { mEmean = mean(mE$Iw)}  else if(pset$imodel==2) { mEmean = mean(mE$Hw)} 
+##sd of normal likelihood for NB data
+if (pset$imodel==1) {
+  mEmean = mean(mE$Iw)} else { 
+  mEmean = mean(mE$Hw)} 
 dev = sqrt(mEmean + mEmean^2*pbin)
 thetaTrue[length(thetaTrue)] = dev
 
 N  = pars$Npop
-rE = parsE$pdm#1#
-rM = pars$pdm#1#
-if(pset$iplatform==0) { iseq=seq_along(mE$time) } else { iseq=iweeksmodel }
-  
-dat <- tibble(Weeks  = mE$time[iseq]/7)
-if (pset$imodel==1) { #plot PIwe (or PHw) against DataIw (or DataHw) => treat PIwe (PHw) as mu (muH) = pdm*Mean
-  dat <- tibble(dat,  PIwe = rE* mE$Iw[iseq]/N,  DataIw = zd/N) } else if (pset$imodel==2) {
-  dat <- tibble(dat,  PHwe =     mE$Hw[iseq]/N,  DataHw = zd/N)
-  #dat <- tibble(dat,  PHwe = rE* mE$Hw[iseq]/N,  DataHw = zd/N)
-  dat <- tibble(dat,  PDwe = rE* mE$Dw[iseq]/N,  DataDw = wd/N) }
+rE = parsE$pdm #1#
+rM = pars$pdm  #1#
+if(pset$iplatform==0) { 
+  iseq  = seq_along(mE$time) 
+  iseqH = iseq
+  iseqD = iseq                    } else { 
+  if (pset$imodel==1) {
+  iseq = iweeksmodel  } else { 
+  iseqH= iweeksmodelH 
+  iseqD= iweeksmodelD }           }
 
-if (pset$iplatform==0) {
-  if (pset$imodel==1) { 
-  dat <- tibble(dat,  PIw = rM* datM$PIw[iseq]) } else if (pset$imodel==2) {
-  dat <- tibble(dat,  PHw =     datM$PHw[iseq])
-  #dat <- tibble(dat,  PHw = rM* datM$PHw[iseq])
-  dat <- tibble(dat,  PDw = rM* datM$PDw[iseq]) }   }
+
+if (pset$imodel==1) { 
+  dat  <- tibble(Weeks  = mE$time[iseq]/7,
+                 PIwe   = rE* mE$Iw[iseq]/N,   #treat PIwe (PHwe) as mu (muH) = pdm*Mean
+                 DataIw = zd/N) 
+  if (pset$iplatform==0) {
+  dat  <- tibble(dat, 
+                 PIw    = rM* datM$PIw[iseq]) }
+    
+ } else if (pset$imodel==2) {
+  datH <- tibble(Weeks  = mE$time[iseqH]/7,  
+                 PHwe   =     mE$Hw[iseqH]/N,  
+                 DataHw = zd/N)
+  datD <- tibble(Weeks  = mE$time[iseqD]/7,  
+                 PDwe   = rE* mE$Dw[iseqD]/N,  
+                 DataDw = wd/N)                
+
+  if (pset$iplatform==0) {
+  datH <- tibble(datH, 
+                 PHw    = datM$PHw[iseqH])
+  datD <- tibble(datD, 
+                 PDw    = datM$PDw[iseqD])    }
+  }
+
 
 
 ## Plots
@@ -182,41 +222,62 @@ correlationPlot(out)
 
 colors <- c("DIw"     = "black", "PIw_est" = "red",   "PIw_mod" = "green",
             "DHw"     = "black", "PHw_est" = "red",   "PHw_mod" = "green",
-            "DDw"     = "black", "PDw_est" = "orange","PDw_mod" = "blue")
+            "DDw"     = "grey", "PDw_est" = "orange","PDw_mod" = "blue")
 
-  if (pset$imodel==1) {Ylab= 'Infections per week'} else {Ylab='hospitalisations & deaths per week'}
+par(mfrow = c(1,2))
 
-  p1 <- ggplot(dat, aes(x= Weeks)) +
-        labs(x = 'Weeks', y = Ylab, color = "Legend") + 
-        scale_color_manual(values = colors)
-
-  if (pset$imodel==1) {
-  p1 <- p1 + geom_point(aes(y=DataIw, color = "DIw")) +
-             geom_line (aes(y=PIwe,   color = "PIw_est")) +
-  if (pset$iplatform==0) {
-  p1 <- p1 + geom_line (aes(y=PIw,    color = "PIw_mod"))  }          } else if(pset$imodel==2) {
-
-  p1 <- p1 + geom_point(aes(y=DataHw, color = "DHw")) +
-             geom_line (aes(y=PHwe,   color = "PHw_est")) +
-             geom_point(aes(y=DataDw, color = "DDw")) +
-             geom_line (aes(y=PDwe,   color = "PDw_est"))
-  if (pset$iplatform==0) {
-  p1 <- p1 + geom_line (aes(y=PHw,    color = "PHw_mod")) +
-             geom_line (aes(y=PDw,    color = "PDw_mod"))  }          }
-  print(p1)
+if (pset$imodel==1) {
+    p1 <- ggplot(dat, aes(x= Weeks)) +
+          labs(x = 'Weeks', y = 'Infections per week', color = "Legend") + 
+          scale_color_manual(values = colors) +
+          geom_point( aes(y=DataIw,   color = "DIw")) +
+          geom_line ( aes(y=PIwe,     color = "PIw_est")) +
+    if (pset$iplatform==0) {
+    p1 <- p1 + geom_line (aes(x=Weeks, y=PIw,  color = "PIw_mod"))  }          
+}
+  
+if(pset$imodel==2) {
+    p1 <- ggplot() +
+          labs(x = 'Weeks', y = 'hospitalisations & deaths per week', color = "Legend") + 
+          scale_color_manual(values = colors) +
+          geom_point(data=datH, aes(x=Weeks,y=DataHw, color = "DHw"), size = 1.4, pch = 19) +
+          geom_line (data=datH, aes(x=Weeks,y=PHwe,   color = "PHw_est")) +
+          geom_point(data=datD, aes(x=Weeks,y=DataDw, color = "DDw"), size =   1,   pch = 16) +
+          geom_line (data=datD, aes(x=Weeks,y=PDwe,   color = "PDw_est"))
+    if (pset$iplatform==0) {
+    p1 <- p1 + geom_line(data=datH, aes(x=Weeks,y=PHw, color = "PHw_mod")) +
+               geom_line(data=datD, aes(x=Weeks,y=PDw, color = "PDw_mod"))  }
+}
+print(p1)
 
 dev.off()
 
 ## pdf: data frame
 pdf(file = paste0(output_dir,"/",pset$File_fit_variables), height=nrow(mE)/3)
-
 if (pset$imodel==1) {gridExtra::grid.table(round(mE[c("time","St","It","Iw","Rt"     )])) }
 if (pset$imodel==2) {gridExtra::grid.table(round(mE[c("time","St","Ht","Hw","Dt","Dw")])) }
 dev.off()
 
+pdf(file = paste0(output_dir,"/",pset$File_fit_data1), height=nrow(datDH)/3)
+if (pset$imodel==1) {
+  gridExtra::grid.table(datD[c("Weeks","Dataz")]) }
+if (pset$imodel==2) {
+  gridExtra::grid.table(datDH[c("Weeks","Dataz")]) }
+dev.off()
+
+if (pset$imodel==2) {
+pdf(file = paste0(output_dir,"/",pset$File_fit_data2), height=nrow(datDD)/3)
+  gridExtra::grid.table(datDD[c("Weeks","Dataw")])
+dev.off() }
 
 ## Summary - output
 sink(file = paste0(output_dir,"/",pset$File_fit_summary),append=TRUE,split=FALSE)
+if (pset$imodel==1) {
+  print(paste0("#data pts fitted: ", length(iweeksmodel))) 
+} else if (pset$imodel==2) {
+  if(pset$iplatform==0) { iwH=iweeksmodel; iwD=iweeksmodel} else {iwH=iweeksmodelH; iwD=iweeksmodelD}
+  print(paste0("#H data pts fitted: ", length(iwH)))
+  print(paste0("#D data pts fitted: ", length(iwD))) }
 print(summary(out)); cat("\n")
 if(pset$iplatform==0) print(paste0("Expected: ", c("rEI = ","rIR = ", "R0 = ", "pE0 = ", "pdm = ", "var = "), round(thetaTrue,3)))
 #if(pset$iplatform==0) print(paste0("Expected: ", c("rEI = ","rIR = ", "R0 = ", "pE0 = ", "var = "), round(thetaTrue,3)))
@@ -228,5 +289,8 @@ print(names(out[[1]]))
 print(names(out[[2]]))
 cat("\n"); cat("\n")
 sink()
+
+
+
 
 ######## 2 Basic Metropolis Hastings MCMC ####
--- modelling/input/SEIRmodel_setup.r
+++ modelling/input/SEIRmodel_setup.r
@@ -3,7 +3,7 @@ pset <- within(pset, {
     TODAY <- format(Sys.Date(), "%d-%m-%Y")
     TIME  <- format(Sys.time(),'%H.%M.%S_%d-%m-%Y')
     ## Job no.
-    Job="J4_" #J1=lm, J2=fit-simul, J3=SEIR_fit
+    Job="J4b_" #J1=lm, J2=fit-simul, J3=SEIR_fit
     ## Model choice
     MODEL    <- c("SEIR", "SEIUHRD");
     imodel   <- 2#1; #2
@@ -35,6 +35,8 @@ pset <- within(pset, {
     File_model_sim_data  <- paste0(Job,"Simul_Incidence.csv")
     File_fit_output      <- paste0(Job,"Fit_MCMC_BT.pdf")
     File_fit_variables   <- paste0(Job,"Fit_Variables_estimated.pdf")
+    File_fit_data1       <- paste0(Job,"Fit_Variables_data1.pdf")
+    File_fit_data2       <- paste0(Job,"Fit_Variables_data2.pdf")
     File_fit_summary     <- paste0(Job,"Fit_Bayesian_report.txt")
     File_data_hosp       <- paste0(Job,"covid_hosp_over_time2again.pdf")
     File_data_deaths     <- paste0(Job,"covid_deaths_over_time2again.pdf")
