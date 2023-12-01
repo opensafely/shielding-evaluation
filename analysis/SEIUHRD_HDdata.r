106752c9b9b7a2060841116e4e5c0ebebedd69bf
Johnny Filipe <John.Filipe@lshtm.ac.uk>
2023-12-01 21:46:58 +0000

    HD data analysis, print DAT wider

--- analysis/SEIUHRD_HDdata.r
+++ analysis/SEIUHRD_HDdata.r
@@ -91,7 +91,7 @@ if(HDaggregated==1){ #} & dim_sc[1]>1000){
 #DATasw <- DAT %>% filter(!is.na(admission_date)) %>% frqs(c(weekH, age_cat, shielding_v1_binary), "weekfreqHas") %>% 
 #Aggregate H (NA D or not), and D (NA H or not)
 
-  DAT %>% select(-c(ons_death_date, shielding_v1_startdate)) %>% arrange(age_cat) %>% print(n=20)
+  DAT %>% select(-c(patient_id, ons_death_date, shielding_v1_binary, shielding_v1_startdate)) %>% arrange(age_cat) %>% print(n=20)
 
 DATasw <- DAT %>% frqs(c(weekH, age_cat, shielding_v1_binary), "weekfreqHas") %>% 
     frqs(c(weekD, age_cat, shielding_v1_binary), "weekfreqDas") %>% 
