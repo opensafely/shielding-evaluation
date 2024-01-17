e825ca2c664955ad78f6814f3f6b77999c4f8fb9
Johnny Filipe <John.Filipe@lshtm.ac.uk>
2024-01-17 22:59:02 +0000

    Qns minor, all data group_by, week-pt ranges

--- analysis/runmkd.R
+++ analysis/runmkd.R
@@ -11,4 +11,4 @@ jobno = "JDat10_"
 
 rmarkdown::render(input=paste0(getwd(),"/analysis/HDdata.Rmd"), clean = T, 
                   output_dir = paste0(getwd(),"/output/HDsynthesis"), 
-                  output_file = paste0(jobno,"HDdata.html") )
+                  output_file = paste0(jobno,"Report_HDdata.html") )
