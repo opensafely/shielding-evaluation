7ddfbccc8390c4656488498e4b64485d477c186e
Johnny Filipe <John.Filipe@lshtm.ac.uk>
2024-04-02 21:02:58 +0100

    fit prevalence revised data-reading

--- modelling/input/setup.r
+++ modelling/input/setup.r
@@ -3,15 +3,15 @@ pset <- within(pset, {
     TODAY <- format(Sys.Date(), "%d-%m-%Y")
     TIME  <- format(Sys.time(),'%H.%M.%S_%d-%m-%Y')
     ## Job no.
-    Job="J5nHbDhmdy2_" #J1=lm, J2=fit-simul, J3=SEIR_fit, J4...=SEIUHRD fits, J5 - age model
-
+    Job="J5nHbDfitP3_" #J1=lm, J2=fit-simul, J3=SEIR_fit, J4...=SEIUHRD fits, J5 - age model
+    
     ## Model choice
     MODEL    <- c("SEIR", "SEIUHRD");
     imodel   <- 2#1; #2
 
     ### OpenSafely
     PLATFORM <- c("Simulate", "Fit dummy data", "OpenSafely"); #0, 1, 2
-    iplatform<- 2#1#0#
+    iplatform<- 2#0#1#
     
     ### model fitting
     DOfit    <- 1; #(0=Dont fit, 1=simulated or OS data)
@@ -33,7 +33,10 @@ pset <- within(pset, {
     File_parameters      <- paste0(MODEL[imodel],"_parameters.r")
     File_parametersas    <- paste0(MODEL[imodel],"as_parameters.r")
     File_contact_data    <- "Contact_matrix_year-from-27Jan20_vector-lenght-9x9x52_norm=maxEV1.csv"
-
+    File_contact_data_0  <- "Contact_matrix_year-from-27Jan20_vector-lenght-9x9x52_norm=maxEV1_0.csv"
+    File_contact_data_1  <- "Contact_matrix_year-from-27Jan20_vector-lenght-9x9x52_norm=maxEV1_1.csv"
+    File_contact_datax3  <- "Contact_matrix_year-from-27Jan20_vector-lenght-9x9x52_norm=maxEV1_x3.csv"
+    
     ### output file names
     File_run             <- paste0(Job,"Simul_run.txt")
     File_contact_summary <- paste0(Job,"Contact_matrix_year-start-27Jan20_stats.txt")
